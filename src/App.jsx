8132c5d8c91f4897719bd9fe80538f10f55407d5
Lonejyc <jocelyn.marcilloux@gmail.com>
2024-10-08 17:28:17 +0200

    SunTimings component finished

--- src/App.jsx
+++ src/App.jsx
@@ -37,7 +37,7 @@ const App = () => {
           <WeatherCard data={data}/>
           <TemperatureChart />
           <WeeklyForecast />
-          <SunTimings />
+          <SunTimings data={data}/>
           <WindInfo />
           <WeatherAlert />
           <PrecipitationLevel />
--- src/components/SunTimings.jsx
+++ src/components/SunTimings.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
-function SunTimings() {
+import Sunrise from '../assets/images/Sunrise.png';
+import Sunset from '../assets/images/Sunset.png';
+
+const SunTimings = ({ data }) => {
+
+  const SunriseH = data.sys ? new Date(data.sys.sunrise * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : "6h43";
+  const SunsetH = data.sys ? new Date(data.sys.sunset * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) : "19h15";
+
   return (
     <div className="sun-timings">
-      <h3>Aujourd'hui</h3>
-      <p>Lever: 6h40</p>
-      <p>Coucher: 17h15</p>
+      <h3>Soleil</h3>
+      <div className="sunrise">
+        <img src={Sunrise} className='icon' alt="Sunrise Icon" />
+        <div className="content">
+          <p className='text'>Lever</p>
+          <p className='time'>{SunriseH}</p>
+        </div>
+      </div>
+      <div className="sunset">
+        <img src={Sunset} className='icon' alt="Sunset Icon" />
+        <div className="content">
+          <p className='text'>Coucher</p>
+          <p className='time'>{SunsetH}</p>
+        </div>
+      </div>
     </div>
   );
 }
